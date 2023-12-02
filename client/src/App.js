2017ecf743b344c89884a427801c7e48abb24e89
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2023-12-02 10:58:09 +0530

    Committed whole project

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import HomePage from "./pages/homepage";
+import Navbar from "./components/universal/Navbar";
+import Auth from "./pages/auth";
+import Footer from "./components/universal/Footer";
+import Services from "./pages/services";
+import OpenRoute from "./components/core/auth/OpenRoute";
+import PrivateRoute from './components/core/auth/PrivateRoute'
+import OTP from "./pages/otp";
+import Error from "./pages/error";
+import { Dashboard } from "./pages/dashboard";
+import Providers from "./components/core/providers/providers";
+import NewProvider from "./components/core/providers/provider/newProvider";
+import ProviderDetails from "./components/core/providerDetails/providerDetails";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-Montserrat">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<Services />} />
+        <Route
+          path="/auth"
+          element={
+            <OpenRoute>
+              <Auth />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="/verify-email"
+          element={
+            <OpenRoute>
+              <OTP />
+            </OpenRoute>
+          }
+        />
+        <Route path="/dashboard" element={
+          <PrivateRoute>
+            <Dashboard/>
+          </PrivateRoute>
+        }/>
+        <Route path='/providers/:id' element={
+          <PrivateRoute>
+            <Providers/>
+          </PrivateRoute>
+        }/>
+        <Route path='/providers/provider/:id' element={
+          <PrivateRoute>
+            <ProviderDetails/>
+          </PrivateRoute>
+        }/>
+         <Route path='/create-provider' element={
+          <PrivateRoute>
+            <NewProvider/>
+          </PrivateRoute>
+        }/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
